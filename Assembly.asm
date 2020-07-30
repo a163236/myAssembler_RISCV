ef4f4e8b5f051e52816509e92f3bae95814cc01e
a163236 <51395204+a163236@users.noreply.github.com>
2020-07-31 00:04:40 +0900

    home commit

--- Assembly.asm
+++ Assembly.asm
@@ -1,16 +1,2 @@
-li x1,10
-li x2,1
-blt x2,x1,12
-mv x2,x1
-j 40
-li x2,1
-li x3,1
-li x4,2
-bge x4,x1,24
-mv x5,x2
-add x2,x2,x3
-mv x3,x5
-addi x4,x4,1
-j -20
-mv x6,x2
+addi x7, a0, 12
 sw x7, 0(x0)
